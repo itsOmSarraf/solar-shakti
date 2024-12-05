4d749bc8e1bc26d0075f61ed194c8f9cb9cac92c
Om Sarraf <itsomsarraf+github@gmail.com>
2024-12-06 03:13:07 +0530

    light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -181,7 +181,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
